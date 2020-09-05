e5c20171813d64e8c548fa2e4f90d11524058d1d
Jeffrey McDonald <jmcdonald@cpp.edu>
2020-09-04 19:18:13 -0700

    "finished up pong"

--- CS3750 - Computers and Society/main.py
+++ CS3750 - Computers and Society/main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 pygame.init()
 
@@ -42,44 +42,98 @@ class rect:
         return self.height
 
     def setX(self,new_x_pos):
-         self.x = new_x_pos
+        self.x = new_x_pos
+
+class circle:
+    def __init__(self,x,y,radius):
+        self.x = x
+        self.y = y
+        self.radius =radius
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def getRadius(self):
+        return self.radius
+
+    def setX(self,new_x):
+        self.x = new_x
+
+    def setY(self,new_y):
+        self.y = new_y
+
 
 
 top_bar = rect(300,0,200,50)
 bottom_bar = rect(300,750,200,50)
-
+ball = circle(300,300,40)
 top_bar_velocity = 0
 bottom_bar_velocity = 0
+ball_velocity_y = -5
+ball_velocity_x = 0
 while run:
+
+    # handling quit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
     clock.tick(80)
     WIN.fill(WHITE)
 
     pygame.draw.rect(WIN, BLUE, (top_bar.getX(), top_bar.getY(), top_bar.get_width(), top_bar.get_height()))
+    pygame.draw.circle(WIN,RED,(ball.getX(),ball.getY()),ball.getRadius(),ball.getRadius())
     pygame.draw.rect(WIN, BLUE, (bottom_bar.getX(), bottom_bar.getY(), bottom_bar.get_width(), bottom_bar.get_height()))
 
+    # movement for both bars
     top_bar.setX(top_bar.getX() + top_bar_velocity)
     bottom_bar.setX(bottom_bar.getX()+bottom_bar_velocity)
+    ball.setX(ball.getX()+ball_velocity_x)
+    ball.setY(ball.getY()+ball_velocity_y)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+
 
 
     keystate = pygame.key.get_pressed()
 
+    if top_bar.getX()<0:
+        top_bar.setX(0)
+    if top_bar.getX()>600:
+        top_bar.setX(600)
+
+    if bottom_bar.getX()<0:
+        bottom_bar.setX(0)
+    if bottom_bar.getX()>600:
+        bottom_bar.setX(600)
+
+
+    if ball.getX()<0:
+        ball_velocity_x=3
+
+    if ball.getX()>800:
+        ball_velocity_x=-3
+
+    if ball.getY() <= top_bar.getY()+top_bar.get_height() and ball.getX()>=top_bar.getX() and  ball.getX()<=top_bar.getX()+top_bar.get_width():
+        ball_velocity_x = random.randrange(-5,4)
+        ball_velocity_y=5
+
+
+    if ball.getY() >=bottom_bar.getY()-bottom_bar.get_height()+30 and ball.getX()>=bottom_bar.getX() and ball.getX()<=bottom_bar.getX()+bottom_bar.get_width():
+        ball_velocity_x = random.randrange(-5,4)
+        ball_velocity_y=-5
+
     if not keystate[pygame.K_LEFT] or not keystate[pygame.K_RIGHT]:
         top_bar_velocity = 0
-
-    if top_bar.getX()*200>=800 or top_bar.getX()<=0:
-        top_bar_velocity = 0
-        print('top_bar x:'+str(top_bar.getX()))
-        print('top_bar v:'+str(top_bar_velocity))
 
     if not keystate[pygame.K_a] or not keystate[pygame.K_d]:
         bottom_bar_velocity = 0
 
+
     if keystate[pygame.K_a]:
-        bottom_bar_velocity = -5
+        if bottom_bar.getX()>0 or bottom_bar.getX()<=800:
+            bottom_bar_velocity = -5
 
     if keystate[pygame.K_d]:
         bottom_bar_velocity = 5
@@ -87,13 +141,9 @@ while run:
     if keystate[pygame.K_LEFT]:
         top_bar_velocity = -5
 
+
     if keystate[pygame.K_RIGHT]:
         top_bar_velocity = 5
-
-
-
-
-
 
     pygame.display.update()
 pygame.quit()
